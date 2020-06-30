98f377ddbb58060ad1cb288042d11bb0fb92dbc9
Enol <grimgaiden@gmail.com>
2020-06-30 19:06:26 +0200

    Set MiServicio.java as primary

--- src/main/java/com/bolsadeideas/springboot/app/springbootdi/models/services/MiServicio.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootdi/models/services/MiServicio.java
@@ -1,12 +1,14 @@
 package com.bolsadeideas.springboot.app.springbootdi.models.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component("miServicioSimple")
+@Primary
 public class MiServicio implements Servicio{
     
     @Override
     public String operacion() {
-        return "ejecutando algún proceso importante...";
+        return "ejecutando algún proceso importante simple...";
     }
 }
---
+++ src/main/java/com/bolsadeideas/springboot/app/springbootdi/models/services/MiServicioComplejo.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.app.springbootdi.models.services;
+
+import org.springframework.stereotype.Component;
+
+@Component("miServicioComplejo")
+public class MiServicioComplejo implements Servicio{
+    
+    @Override
+    public String operacion() {
+        return "ejecutando algún proceso importante complicado...";
+    }
+}
