733571801bab3b86dcac31cae30088ff5b393c41
Enol <grimgaiden@gmail.com>
2020-06-30 19:11:39 +0200

    Qualifier used for IndexController.java constructor

--- src/main/java/com/bolsadeideas/springboot/app/springbootdi/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootdi/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.bolsadeideas.springboot.app.springbootdi.controllers;
 import com.bolsadeideas.springboot.app.springbootdi.models.services.Servicio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ public class IndexController {
     private Servicio servicio;
 
     @Autowired
-    public IndexController(Servicio servicio) {
+    public IndexController(@Qualifier("miServicioComplejo") Servicio servicio) {
         this.servicio = servicio;
     }
 
